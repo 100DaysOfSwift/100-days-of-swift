1bd5cbf24ba90b2c28f12a120461c2bb5790b3ee
zdodev <iver0945@gmail.com>
2023-08-29 23:17:20 +0900

    📖: day 1 노트

---
+++ 이명진/Day1/Day1.playground/Contents.swift
@@ -0,0 +1,57 @@
+import UIKit
+
+var str = "Hello, playground"
+str = "Goodbye"
+
+var favoriteShow = "Orange is New Black"
+favoriteShow = "The Good Place"
+favoriteShow = "Doctor Who"
+
+var age = 38 // 정수
+var population = 8_000_000 // 정수
+
+var meaningOfLife = 42
+
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+var quote = "Change the world by being yourself"
+
+var burns = """
+The best laid schemes
+O’ mice and men
+Gang aft agley
+"""
+
+var pi = 3.141
+
+var awesome = true
+
+var myInt = 1
+var myDouble = 1.0
+
+var score = 85
+var str3 = "Your score was \(score)"
+var results = "The test results are here: \(str3)"
+
+let taylor = "swift"
+
+let str4 = "Hello, playground"
+
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
+
+var percentage: Double = 99
+
+var name: String
