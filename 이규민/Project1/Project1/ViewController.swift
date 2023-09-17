6fb0c844c4b58ca578917fc0dde6a8db6d55f14c
Gyumin Lee <gyumin.q.lee@gmail.com>
2023-09-17 23:56:36 +0900

    add: [Day16] Project1

---
+++ 이규민/Project1/Project1/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  project1
+//
+//  Created by 이규민 on 2023/09/16.
+//
+
+import UIKit // this file will reference the iOS user interface toolkit.
+
+class ViewController: UIViewController {
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+
+        for item in items {
+            if item.hasPrefix("nssl") {
+                // this is a picture to load!
+                pictures.append(item)
+            }
+        }
+        print(pictures)
+    }
+}
