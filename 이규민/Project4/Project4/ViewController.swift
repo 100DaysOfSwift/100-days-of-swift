b4061a99152c92b0b893f5d5ffb8c3e057e366dd
Gyumin Lee <gyumin.q.lee@gmail.com>
2023-10-01 01:33:00 +0900

    add: [Day24] Pjt4-WKWebView

---
+++ 이규민/Project4/Project4/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by 이규민 on 2023/10/01.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+    }
+
+
+}
+
