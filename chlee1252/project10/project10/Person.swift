a7fabda6d05766159e0dd31872c29936d00dcf94
Marc Lee <chlee1252@gmail.com>
2023-10-11 00:20:48 +0900

    feat: add project10

---
+++ chlee1252/project10/project10/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  project10
+//
+//  Created by Changhwan Lee on 2023/10/11.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+
+}
