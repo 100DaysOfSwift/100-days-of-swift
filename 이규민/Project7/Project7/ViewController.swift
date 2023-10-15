92d3852ab8318b0866832f91281979953a8f0daf
Gyumin Lee <gyumin.q.lee@gmail.com>
2023-10-16 00:46:32 +0900

    add: [Day34] Pjt7-loadHTMLString

--- 이규민/Project7/Project7/ViewController.swift
+++ 이규민/Project7/Project7/ViewController.swift
@@ -41,11 +41,16 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for:indexPath)
-        
         let petition = petitions[indexPath.row]
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
     }
 
 
