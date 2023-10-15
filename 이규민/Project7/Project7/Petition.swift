9cfe3919cd83a9c095439a55a701e875f11b984f
Gyumin Lee <gyumin.q.lee@gmail.com>
2023-10-15 23:59:39 +0900

    add: [Day33] Pjt7-Parsing JSON

---
+++ 이규민/Project7/Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by 이규민 on 2023/10/15.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ 이규민/Project7/Project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by 이규민 on 2023/10/15.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
