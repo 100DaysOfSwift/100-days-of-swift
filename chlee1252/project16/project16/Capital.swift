728d8b67f23e35bf80105bdcd3f506a6ca2662fc
Marc Lee <chlee1252@gmail.com>
2023-11-07 22:27:52 +0900

    feat: add project16

---
+++ chlee1252/project16/project16/Capital.swift
@@ -0,0 +1,22 @@
+//
+//  Capital.swift
+//  project16
+//
+//  Created by Changhwan Lee on 2023/11/07.
+//
+
+import MapKit
+import UIKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String? = nil, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+
+}
