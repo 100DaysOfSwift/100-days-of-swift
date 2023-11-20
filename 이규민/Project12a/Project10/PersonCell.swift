d51288054e241c5752d8fa92020ab8f9a2e86b50
Gyumin Lee <gyumin.q.lee@gmail.com>
2023-11-20 23:58:16 +0900

    add: [Day48] Pjt12-UserDefaults

---
+++ 이규민/Project12a/Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by 이규민 on 2023/10/30.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var name: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    
+}
